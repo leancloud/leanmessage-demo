cf33a65a6a1e29aa376eea22a89052e24e428e1b
leeyeh <lee55962698@gmail.com>
2018-03-23 16:17:54 +0800

    fix: 更新表情图片域名

--- Web/src/configs.js
+++ Web/src/configs.js
@@ -6,18 +6,18 @@ export const stickers = [
     'emoji-sticker',
     // group stickers
     [
-      ['happy', 'https://dn-KNtFGfqM.qbox.me/0f5c5ce64dbdee015f3e.svg'],
-      ['laughing', 'https://dn-KNtFGfqM.qbox.me/5cf0dd10f240f66fd779.svg'],
-      ['cool', 'https://dn-KNtFGfqM.qbox.me/3a0eca4aa36cf7603c0e.svg'],
-      ['laughing-with-tears', 'https://dn-KNtFGfqM.qbox.me/8ff3ab43c86d721e64a8.svg'],
-      ['confused', 'https://dn-KNtFGfqM.qbox.me/7460b8e0f2302a1e527d.svg'],
-      ['crying', 'https://dn-KNtFGfqM.qbox.me/0b2c45cd6ccbb9ad7796.svg'],
-      ['surprised', 'https://dn-KNtFGfqM.qbox.me/aa91b0df2b5d7b98695d.svg'],
-      ['shocked', 'https://dn-KNtFGfqM.qbox.me/4025addb60eb26095cd0.svg'],
-      ['sad', 'https://dn-KNtFGfqM.qbox.me/ae6e286448b009f9a0cf.svg'],
-      ['angry', 'https://dn-KNtFGfqM.qbox.me/df6d6f03c316a5d008f1.svg'],
-      ['sick', 'https://dn-KNtFGfqM.qbox.me/4f6eb7f95f750b2eff3a.svg'],
-      ['greed', 'https://dn-KNtFGfqM.qbox.me/61a83f01b63573fa2dc4.svg']
+      ['happy', 'https://lc-kntfgfqm.cn-n1.lcfile.com/0f5c5ce64dbdee015f3e.svg'],
+      ['laughing', 'https://lc-kntfgfqm.cn-n1.lcfile.com/5cf0dd10f240f66fd779.svg'],
+      ['cool', 'https://lc-kntfgfqm.cn-n1.lcfile.com/3a0eca4aa36cf7603c0e.svg'],
+      ['laughing-with-tears', 'https://lc-kntfgfqm.cn-n1.lcfile.com/8ff3ab43c86d721e64a8.svg'],
+      ['confused', 'https://lc-kntfgfqm.cn-n1.lcfile.com/7460b8e0f2302a1e527d.svg'],
+      ['crying', 'https://lc-kntfgfqm.cn-n1.lcfile.com/0b2c45cd6ccbb9ad7796.svg'],
+      ['surprised', 'https://lc-kntfgfqm.cn-n1.lcfile.com/aa91b0df2b5d7b98695d.svg'],
+      ['shocked', 'https://lc-kntfgfqm.cn-n1.lcfile.com/4025addb60eb26095cd0.svg'],
+      ['sad', 'https://lc-kntfgfqm.cn-n1.lcfile.com/ae6e286448b009f9a0cf.svg'],
+      ['angry', 'https://lc-kntfgfqm.cn-n1.lcfile.com/df6d6f03c316a5d008f1.svg'],
+      ['sick', 'https://lc-kntfgfqm.cn-n1.lcfile.com/4f6eb7f95f750b2eff3a.svg'],
+      ['greed', 'https://lc-kntfgfqm.cn-n1.lcfile.com/61a83f01b63573fa2dc4.svg']
     ],
     // optional notes
     ['designed by freepik from Flaticon', 'https://www.flaticon.com/authors/freepik']
@@ -25,18 +25,18 @@ export const stickers = [
   [
     'animal',
     [
-      ['dog', 'https://dn-KNtFGfqM.qbox.me/fc4de126619d4839af70.svg'],
-      ['cat', 'https://dn-KNtFGfqM.qbox.me/29c540f5f272d63461e9.svg'],
-      ['panda-bear', 'https://dn-KNtFGfqM.qbox.me/1e39bb10e7f02379cf9e.svg'],
-      ['lion', 'https://dn-KNtFGfqM.qbox.me/260bcf78faa9e915f230.svg'],
-      ['tiger', 'https://dn-KNtFGfqM.qbox.me/a2996e17cd43a471b305.svg'],
-      ['koala', 'https://dn-KNtFGfqM.qbox.me/d929b981e6896c034d3c.svg'],
-      ['giraffe', 'https://dn-KNtFGfqM.qbox.me/7be12ba23b2adc6c1391.svg'],
-      ['frog', 'https://dn-KNtFGfqM.qbox.me/08471c8b3367ee175acd.svg'],
-      ['rabbit', 'https://dn-KNtFGfqM.qbox.me/37cb33111be9c6cd0c75.svg'],
-      ['raccoon', 'https://dn-KNtFGfqM.qbox.me/48fc79f68974c621c443.svg'],
-      ['bear', 'https://dn-KNtFGfqM.qbox.me/b85a37ff48219827d71b.svg'],
-      ['fox', 'https://dn-KNtFGfqM.qbox.me/20c1ad98ba2af71ec9c5.svg']
+      ['dog', 'https://lc-kntfgfqm.cn-n1.lcfile.com/fc4de126619d4839af70.svg'],
+      ['cat', 'https://lc-kntfgfqm.cn-n1.lcfile.com/29c540f5f272d63461e9.svg'],
+      ['panda-bear', 'https://lc-kntfgfqm.cn-n1.lcfile.com/1e39bb10e7f02379cf9e.svg'],
+      ['lion', 'https://lc-kntfgfqm.cn-n1.lcfile.com/260bcf78faa9e915f230.svg'],
+      ['tiger', 'https://lc-kntfgfqm.cn-n1.lcfile.com/a2996e17cd43a471b305.svg'],
+      ['koala', 'https://lc-kntfgfqm.cn-n1.lcfile.com/d929b981e6896c034d3c.svg'],
+      ['giraffe', 'https://lc-kntfgfqm.cn-n1.lcfile.com/7be12ba23b2adc6c1391.svg'],
+      ['frog', 'https://lc-kntfgfqm.cn-n1.lcfile.com/08471c8b3367ee175acd.svg'],
+      ['rabbit', 'https://lc-kntfgfqm.cn-n1.lcfile.com/37cb33111be9c6cd0c75.svg'],
+      ['raccoon', 'https://lc-kntfgfqm.cn-n1.lcfile.com/48fc79f68974c621c443.svg'],
+      ['bear', 'https://lc-kntfgfqm.cn-n1.lcfile.com/b85a37ff48219827d71b.svg'],
+      ['fox', 'https://lc-kntfgfqm.cn-n1.lcfile.com/20c1ad98ba2af71ec9c5.svg']
     ],
     ['designed by freepik from Flaticon', 'https://www.flaticon.com/authors/freepik']
   ]
